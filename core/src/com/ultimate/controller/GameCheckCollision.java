a7ea8b84018a9ea65894c17d96294e78a53351c3
n3n <s6070069@kmitl.ac.th>
2014-11-14 18:54:28 +0700

    fix bug

--- core/src/com/ultimate/controller/GameCheckCollision.java
+++ core/src/com/ultimate/controller/GameCheckCollision.java
@@ -4,8 +4,6 @@ import java.util.ConcurrentModificationException;
 
 import com.ultimate.game.Player;
 import com.ultimate.game.UltimateFight;
-import com.ultimate.network.StateTime;
-import com.ultimate.screen.GameScreen;
 import com.ultimate.unit.JobClass;
 import com.ultimate.unit.Skill;
 
@@ -20,25 +18,43 @@ public class GameCheckCollision{
 	public void run(){
 			try{
 				for(int skill_id: game.world.getObjectMap().keySet()){
-//					System.out.println("IN");
-					Skill object = game.world.getObjectMap().get(skill_id);
+					final Skill object = game.world.getObjectMap().get(skill_id);
 					
 					for(int player_id: game.world.getPlayersMap().keySet()){
 						final Player player = game.world.getPlayersMap().get(player_id);
 						if((player.getPlayerID() != object.getPlayer_id()) && object.getBounds().overlaps(player.character.getBounds()) && !object.isPassed()){
 							System.out.println("Player: "+player.getPlayerID()+" was attacked by player: "+object.getPlayer_id());
+							game.world.getPlayersMap().get(player.getPlayerID()).character.setHp(game.world.getPlayersMap().get(player.getPlayerID()).character.getHp()-100);
 							new Thread(){
 								public void run(){
 									game.player.setStateTime(0);
 									try {
-										for(int i=1;i<=700;i++){
+										for(int i=1;i<=600;i++){
 											game.world.getPlayersMap().get(player.getPlayerID()).character.setSTATE(JobClass.STATE_HURT);
+											if(i%20 == 0) {
+												if(object.getTurn() == -1) {
+													
+													game.world.getPlayersMap().get(player.getPlayerID()).character.moveLeft(4);
+													if(GameInputProcessor.collision()){
+														game.player.character.moveRight(4);
+														break;
+													}
+												}
+												if(object.getTurn() == 1) {
+													game.world.getPlayersMap().get(player.getPlayerID()).character.moveRight(4);
+													System.out.println();
+													if(GameInputProcessor.collision()){
+														game.player.character.moveLeft(4);
+														break;
+													}
+												}
+												game.world.getPlayersMap().get(player.getPlayerID()).character.moveDown(4);
+												if(GameInputProcessor.collision()) game.player.character.moveUp(4);
+											}
 											game.world.getPlayersMap().get(player.getPlayerID()).character.setAction(true);
 											Thread.sleep(1);
 										}
 										game.world.getPlayersMap().get(player.getPlayerID()).character.setAction(false);
-										
-										
 									} catch (InterruptedException e) {
 										e.printStackTrace();
 									}
